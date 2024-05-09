e5ed4c248ff01b9121c05c3f9530d9a5a9b7dc28
Steven_Villegas <syivenvillegas0@gmail.com>
2024-05-09 11:35:23 -0500

    commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Header} from "./Components/Header/Header";
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+      <h1 className="text-red-700 text-7xl m-8 p-4 " >Hi,World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header className="w-full h-24 bg-sky-300">
+    <div className="w-40 h-8 p-2 m-3 bg-red-700">container navbar</div>
+    <div className="w-40 h-8 p-2 m-3 bg-green-600">container logo</div>
+    </header>
+  )
+}
