60e91f0ac0fb2186eef219218bd0ccaef3de21db
Steven_Villegas <syivenvillegas0@gmail.com>
2024-05-10 11:49:30 -0500

    solucionando errores

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Navbar } from '../Layouts/Navbar/Navbar';
+import { ItemNavbar } from '../UI/ItemNavbar/ItemNavbar';
+
+
 
 export const Header = () => {
   return (
-    <header className="w-full h-24 bg-sky-300">
-    <div className="w-40 h-8 p-2 m-3 bg-red-700">container navbar</div>
-    <div className="w-40 h-8 p-2 m-3 bg-green-600">container logo</div>
+    <header className='flex justify-center items-center flex-grow'>
+      <Navbar>
+        <ItemNavbar content='inicio' />
+        <ItemNavbar content='acerca de mi' />
+        <ItemNavbar content='inicio' />
+        <ItemNavbar content='inicio' />
+
+      </Navbar>
     </header>
   )
 }
